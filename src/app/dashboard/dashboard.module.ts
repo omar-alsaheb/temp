2d7494b4ebd6a1e9837e44168413d875f0ccbe20
Omar Altamimi <OAltamimi@eastnets.com>
2025-02-11 15:12:16 +0300

    add components

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    HomeComponent,
+    SettingsComponent,
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
